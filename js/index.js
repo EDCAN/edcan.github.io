ef5b91152b2218cee07328307a8598cef1548bf9
Jhun24 <jjanghuni102@naver.com>
2017-02-02 00:29:08 +0900

    check device size

--- js/index.js
+++ js/index.js
@@ -17,16 +17,6 @@ $(function(){
 
 
   $(document).ready(function(){
-        var UserAgent = navigator.userAgent;
-
-        if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null)
-        {
-            $(".link").addClass("setBlock");
-            $(".ED_Logo").addClass("maxSize")
-        }else{
-            $(".link").addClass("setFlex");
-        }
-
 
     $.ajax({
       method:"GET",
