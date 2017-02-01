51f0fcc881b16b758db09f16f95ba1fe3721403d
Jhun24 <jjanghuni102@naver.com>
2017-02-01 23:43:13 +0900

    check device type

--- js/index.js
+++ js/index.js
@@ -17,6 +17,23 @@ $(function(){
 
 
   $(document).ready(function(){
+      function isMobile(){
+          var UserAgent = navigator.userAgent;
+
+          if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null)
+          {
+            return true;
+          }else{
+            return false;
+          }
+      }
+
+      if(isMobile()){
+        alert("Mobile")
+      }
+      else{
+        alert("PC")
+      }
 
     $.ajax({
       method:"GET",
